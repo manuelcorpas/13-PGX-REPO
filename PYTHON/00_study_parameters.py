961111bc8ca8e57dc98cf716e4038edcdffe36ab
CambridgePrecisionMedicine <m.corpas@cpm.onl>
2022-12-28 10:13:14 +0000

    uploaded study_parameters

--- PYTHON/00_study_parameters.py
+++ PYTHON/00_study_parameters.py
@@ -1,6 +1,8 @@
 import csv
 import mariadb
 import os
+import re
+import regex
 
 try:
     db = mariadb.connect(host='localhost',user='admin',passwd='root',db='pgx')
@@ -11,5 +13,30 @@ except mariadb.Error as e:
 
 cursor = db.cursor(dictionary=True)
 
-sql1 = "INSERT INTO (
+sql1 = "INSERT INTO 00_study_parameters(Study_Parameters_ID,Variant_Annotation_ID"     \
+     + ",Study_Type,Study_Cases,Study_Controls,Characteristics,Characteristics_Type"   \
+     + ",Frequency_In_Cases,Allele_Of_Frequency_In_Cases,Frequency_In_Controls"        \
+     + ",Allele_Of_Frequency_In_Controls,P_Value,Ratio_Stat_Type,Ratio_Stat"           \
+     + ",Confidence_Interval_Start,Confidence_Interval_Stop,Biogeographical_Groups) "  \
+     + "VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}','{7}','{8}','{9}','{10}'"     \
+     + ",'{11}','{12}','{13}','{14}','{15}','{16}')"                      
+
+os.chdir("INPUT/variantAnnotations")
+myfile = "study_parameters.tsv"
+
+with open(myfile) as f:
+    reader = csv.reader(f,delimiter="\t")
+    next(reader, None)
+    for row in reader:
+        print(row)
+        # replace single quotes
+        my_line = [i.replace("'", " ") for i in row]
+        # replace non latin characters
+        out_line = []
+        for text in my_line:
+            stripped_text = ''
+            for c in text:
+                stripped_text += c if len(c.encode(encoding='utf_8'))==1 else ''
+            out_line.append(stripped_text)
+        cursor.execute(sql1.format(*out_line))
 
--- SQL/00_study_parameters.sql
+++ SQL/00_study_parameters.sql
@@ -1,26 +1,25 @@
 USE `pgx`
 
-DROP TABLE IF EXISTS `lc`.`00_study_parameters` ;
+DROP TABLE IF EXISTS `pgx`.`00_study_parameters` ;
 
-CREATE TABLE IF NOT EXISTS `lc`.`00_study_parameters` (
+CREATE TABLE IF NOT EXISTS `pgx`.`00_study_parameters` (
 	`Study_Parameters_ID`			INT NOT NULL,
 	`Variant_Annotation_ID`			INT NOT NULL,
 	`Study_Type`				TEXT,
-	`Study_Cases`				INT,
-	`Study_Controls`			INT,
-	`Characteristics`			TEXT,
+	`Study_Cases`				TEXT,
+	`Study_Controls`			TEXT,
+	`Characteristics`			TEXT CHARACTER SET utf8,
 	`Characteristics_Type`			TEXT,
-	`Frequency_In_Cases`			FLOAT,
+	`Frequency_In_Cases`			TEXT,
 	`Allele_Of_Frequency_In_Cases`		TEXT,
-	`Frequency_In_Controls`			FLOAT,
+	`Frequency_In_Controls`			TEXT,
 	`Allele_Of_Frequency_In_Controls`	TEXT,
-	`P_Value`				FLOAT,
-	`Ratio_Stat_Type`			VARCHAR(20),
-	`Ratio_Stat`				FLOAT,
-	`Confidence_Interval_Start`		FLOAT,
-	`Confidence_Interval_Stop`		FLOAT,
-	`Biogeographical_Groups`		VARCHAR(255),
-	PRIMARY KEY(Study_Parameters_ID),
-	INDEX(Biogeographical_Groups)
+	`P_Value`				TEXT,
+	`Ratio_Stat_Type`			TEXT,
+	`Ratio_Stat`				TEXT,
+	`Confidence_Interval_Start`		TEXT,
+	`Confidence_Interval_Stop`		TEXT,
+	`Biogeographical_Groups`		TEXT CHARACTER SET utf8,
+	PRIMARY KEY(Study_Parameters_ID)
 	)
 ENGINE = MyISAM;
